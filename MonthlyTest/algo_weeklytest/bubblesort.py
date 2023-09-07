628b1ef8c866fbaeccc17da42f1079a68676d30f
이승집 <edward626kr@naver.com>
2023-09-07 17:14:06 +0900

    a

--- MonthlyTest/algo_weeklytest/bubblesort.py
+++
@@ -1,9 +0,0 @@
-def BubbleSort(arr, N):
-    for i in range(N-1, 0, -1):
-        for j in range(0, i):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
-
-List = [4, 13, 26, 3, 0, 511]
-print(BubbleSort(List, len(List)))
